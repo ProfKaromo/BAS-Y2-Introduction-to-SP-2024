b2a2cb1c356d53a11cf13ba596f46c8f1dad61a8
ProfKaromo <jkaromo@kyu.ac.ke>
2025-09-08 12:34:32 +0300

    Some updates

---
+++ First Class 2025.R
@@ -0,0 +1,8 @@
+#First R code
+
+print("Hello World")
+cat("Hello world")
+
+for (i in 1:5) {
+  cat("Hello world\n")
+}
--- Week 1.R
+++ Week 1.R
@@ -6,3 +6,18 @@ if(FALSE){
   "This is a demo for multi-line comments and it should be put
  inside either a single of double quote"
 }
+
+y <- c(3,6,7,8,4,3)
+mean(y)
+
+library(ggplot2)
+
+# Use stdout as per normal...
+print("Hello, world!")
+
+# Use plots...
+plot(cars)
+
+# Even ggplot!
+qplot(wt, mpg, data = mtcars, colour = factor(cyl))
+
